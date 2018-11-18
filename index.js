95715caae868b50d29ae449929e1e9d0fccbf897
github username <github email address>
2018-11-18 17:00:17 +0000

    Done.

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
-/*
-Variables defined in the test suite:
-- oldID
-- currentAge
+const newID= 1000000000 + oldID;
+const ageIsValid = Number.isInteger(currentAge);
+const randomNumber = Math.random() * 20;
+const randomInteger = Math.floor(randomNumber);
 
-You should NOT try to redefine any of the above variables.
-*/
-
-// Write your code below this line!
+const randomUserID = randomInteger + 1000000001;
